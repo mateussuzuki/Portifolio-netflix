56b5fcd0da5917f22f082f1475498f3ffff02726
mateussuzuki <mateussuzuki2022@hotmail.com>
2023-10-16 22:17:03 -0300

    perfis tela inicial

--- portifolio-netflix-angular/src/app/app.module.ts
+++ portifolio-netflix-angular/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileScreenComponent } from './components/profile-screen/profile-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProfileScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ portifolio-netflix-angular/src/app/components/profile-screen/profile-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-screen',
+  templateUrl: './profile-screen.component.html',
+  styleUrls: ['./profile-screen.component.scss']
+})
+export class ProfileScreenComponent {
+
+}
